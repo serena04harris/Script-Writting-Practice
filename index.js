c42fd9011fdd57bcb1b5038eb81626b221062664
serena04harris <sharri23@uvm.edu>
2024-10-02 10:45:13 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+// Object type: dog
+// properties: 
+    // Name: Kevin
+    // Breed: Corgi
+    // Age: 2 years
+// Events:
+    // Eat food - happens when hungry
+    // Takes a nap - happens when tired
+    // Goes on a walk - happens when energized
+// Methods: 
+    // Gets full when fed
+    // feels engergized after naps
+    // feels happy when walked
